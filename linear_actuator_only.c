539ea7cc1d09e1a8bccf48ebd7e21608750fe9fd
deksioglu <143645097+deksioglu@users.noreply.github.com>
2024-02-04 14:08:56 -0500

    Update linear_actuator_only.c

--- linear_actuator_only.c
+++ linear_actuator_only.c
@@ -1,12 +1,20 @@
-
+const int pwmPin = 9; // PWM pin connected to the driver
+const int actuatorUpPWM = 180; // PWM value for moving up
+const int actuatorDownPWM = 0; // PWM value for moving down
+const long duration = 2000; // Duration for each movement in milliseconds
 
 void setup()
 {
- 
+  pinMode(pwmPin, OUTPUT);
 }
-
 
 void loop()
 {
+  // Move the actuator up
+  analogWrite(pwmPin, actuatorUpPWM);
+  delay(duration); // Wait for the actuator to move
 
+  // Move the actuator down
+  analogWrite(pwmPin, actuatorDownPWM);
+  delay(duration); // Wait for the actuator to move
 }
