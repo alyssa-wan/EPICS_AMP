6eeedfe4cfc0d2b43c80042297af402b7dfca477
sarahdemos <157309823+sarahdemos@users.noreply.github.com>
2024-02-04 13:53:48 -0500

    Update arduino_emg_reader.c

--- arduino_emg_reader.c
+++ arduino_emg_reader.c
@@ -1,6 +1,6 @@
 void setup()
 {
-  Serial.begin(9600)   // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);   // initialize serial communication at 9600 bits per second:
 }
 
 void loop() 
